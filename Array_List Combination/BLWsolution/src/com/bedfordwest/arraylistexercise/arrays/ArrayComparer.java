a95b0b0a7448bdfb616ed47c18f790b2669f9a72
bedfordwest <bedfordwest@gmail.com>
2015-01-23 19:58:32 -0600

    Boggled my brain around for a couple of hours and finally came up with an implementation for an algorithm I think makes sense. If this is slower than simply nesting for loops, then I will be very sad...

--- Array_List Combination/BLWsolution/src/com/bedfordwest/arraylistexercise/arrays/ArrayComparer.java
+++ Array_List Combination/BLWsolution/src/com/bedfordwest/arraylistexercise/arrays/ArrayComparer.java
@@ -1,5 +1,8 @@
 package com.bedfordwest.arraylistexercise.arrays;
 
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+
 /**
  * Created by Bedford on 1/23/2015.
  */
@@ -10,9 +13,8 @@ public final class ArrayComparer
         throw new AssertionError();
     }
 
-    public String outputCommonalities(String[] firstString, int arrayIndex1, String[] secondString, int arrayIndex2)
+    public String outputElementCommonalities(String[] firstString, int arrayIndex1, String[] secondString, int arrayIndex2)
     {
-        String commonCharacters = "";
 
         if (arrayIndex1 > firstString.length)
         {
@@ -24,7 +26,21 @@ public final class ArrayComparer
             System.out.format("Index %i does not exist for the second array!", arrayIndex2);
         }
 
-        return commonCharacters;
+        LinkedList<Character> l1 = new LinkedList<Character>(), l2 = new LinkedList<Character>();
+
+        for (int i = 0; i < firstString[arrayIndex1].length(); i++)
+        {
+            l1.add(firstString[arrayIndex1].charAt(i));
+        }
+
+        for (int j = 0; j < secondString[arrayIndex2].length(); j++)
+        {
+            l2.add(secondString[arrayIndex2].charAt(j));
+        }
+
+        l1.retainAll(l2);
+
+        return l1.toArray(new Character[0]).toString();
     }
 
 }
